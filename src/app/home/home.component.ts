bfbaded7f975fc3117e45f919cc1c83d7d9fe495
Yerko Palma <yerko.palma@usach.cl>
2019-08-15 16:32:29 -0400

    improved tag filtering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 export interface Post {
   title: string;
@@ -22,12 +23,27 @@ export interface Timestamp {
 })
 export class HomeComponent implements OnInit {
   posts: Observable<Post[]>;
+  tags: string[] = [];
 
   constructor(private db: AngularFirestore) {
     this.posts = db.collection<Post>('posts').valueChanges();
   }
 
   ngOnInit() {
+  }
+
+  shouldShow(post: Post) {
+    if (this.tags === null || this.tags.length === 0) { return true; }
+    return this.tags.some(tag => post.tags.indexOf(tag) > -1);
+  }
+
+  sortByTag(tag: string) {
+    if (this.tags.indexOf(tag) < 0) { this.tags.push(tag); }
+  }
+
+  removeTagFiler(tag: string) {
+    // tslint:disable-next-line: variable-name
+    this.tags = this.tags.filter(_tag => _tag !== tag);
   }
 
 }
