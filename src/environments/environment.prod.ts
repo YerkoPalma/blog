1de8629ea14989c01e1bc3d1e62df73d6851d4bb
Yerko Palma <yerko.palma@usach.cl>
2019-08-15 20:07:46 -0400

    correctly publish to gh-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA6SJFoE2zFdqZOle7cXLXsrVivL-OktaY',
+    projectId: 'blog-yp',
+    databaseURL: 'https://blog-yp.firebaseio.com',
+    storgeBucket: 'gs://blog-yp.appspot.com'
+  }
 };
